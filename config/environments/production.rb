6cc0f70b31f6b7be64f365abcaf9864439e5731d
Kyle Wilcox <kwilcox@asascience.com>
2013-06-07 11:50:06 -0400

    Set the :from default on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,7 @@ Matos::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default :from => "matos@asascience.com"
 
   # Enable threaded mode
   # config.threadsafe!
