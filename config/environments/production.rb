052eac7f6d25cf690b941b606bcd4c11429d3a78
Kyle Wilcox <kwilcox@asascience.com>
2013-06-06 23:56:00 -0400

    raise production deliver errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Matos::Application.configure do
   config.action_mailer.default_url_options = { :host => 'matos.asascience.com' }
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.default :from => "matos@asascience.com"
   config.action_mailer.sendmail_settings = { :arguments => '-i -t -f matos@asascience.com' }
